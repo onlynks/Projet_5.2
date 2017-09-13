8d740291da309e8b189362707fa4c4fc8de3150a
onlynks <nicogar12@gmail.com>
2017-09-13 15:24:04 +0000

    file Routes.php added and updating of index.php

---
+++ app/configuration/Routes.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\configuration;
+/**
+ * Contains all the routes for the application
+ *
+ * Class Routes
+ * @package app\configuration
+ */
+class Routes
+{
+	public static $routes = array(
+		array(
+			"pattern"    => "homepage",
+			"controller" => "homepage",
+			"action"     => "index",
+		),
+	);
+}
