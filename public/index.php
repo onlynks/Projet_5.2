6a7efe1bbb3b7c51faa551a6b1cd6e0300898377
onlynks <nicogar12@gmail.com>
2017-09-13 15:22:48 +0000

    file Routes.php added and updating of index.php

--- public/index.php
+++ public/index.php
@@ -3,6 +3,11 @@ define( "APP_PATH", dirname( dirname( __FILE__ ) ) );
 try {
 	require( "../Framework/Core.php" );
 	Framework\Core::initialize();
+	$router = new \Framework\Router();
+	\Framework\Registry::set( "router", $router );
+	foreach ( \app\configuration\Routes::$routes as $route ) {
+		$router->addRoute( $route );
+	}
 } catch ( Exception $e ) {
 	header( "Content-type: text/html" );
 	echo "An error occurred." . $e->getMessage();
