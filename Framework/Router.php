aa2fe0a2dc2adb3d6c73cb63a19be0f3042997a9
onlynks <nicogar12@gmail.com>
2017-09-21 14:20:09 +0000

    checks if the URL matches one of the Routes

--- Framework/Router.php
+++ Framework/Router.php
@@ -1,20 +1,28 @@
 <?php
 namespace Framework;
-
 class Router
-{
+	{
 	protected $_url;
 	protected $_controller;
 	protected $_action;
 	protected $_routes = array();
+	public function __construct( $options = array() ) 
+		{
+			foreach ( $options as $key => $value ) 
+			{
+				$method        = "_" . $key;
+				$this->$method = $value;
+			}
+		}
 	/**
 	 * Adds a route in the Router
 	 *
-	 * @param $route
+	 * @param Route $route
 	 *
 	 * @return $this
 	 */
-	public function addRoute( $route ) 
+	public function addRoute( Route $route ) 
+	
 	{
 		$this->_routes[] = $route;
 		return $this;
@@ -22,18 +30,21 @@ class Router
 	
 	public function removeRoute( $route ) 
 	{
-	    
 	}
 	public function getRoutesByName() 
 	{
-	    
 	}
 	public function getRoutes() 
 	{
-	    
 	}
 	public function dispatch() 
+	
 	{
-	    
+		foreach ( $this->_routes as $route )
+		{
+			if ( $route->matches( $this->_url ) ) 
+			{
+			}
+		}
 	}
 }
