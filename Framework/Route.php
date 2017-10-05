98eda5c5e39e0bd4bffbf65115ca8f1781896636
onlynks <nicogar12@gmail.com>
2017-10-05 13:05:47 +0000

    determine controller and action from the URL

---
+++ Framework/Route.php
@@ -0,0 +1,41 @@
+<?php
+namespace Framework;
+class Route 
+    {
+	protected $_pattern;
+	protected $_controller;
+	protected $_action;
+	public function __construct( $options = array() )
+	    {
+    		foreach ( $options as $key => $value ) 
+        		{
+        			$method        = "_" . $key;
+        			$this->$method = $value;
+        		}
+	    }
+	/**
+	 * Checks if the url matches the Route
+	 *
+	 * @param $url
+	 *
+	 * @return int
+	 */
+	public function matches( $url ) 
+	{
+		return preg_match( "#^{$this->_pattern}$#", $url );
+	}
+	/**
+	 * @return string
+	 */
+	public function getController() 
+	{
+		return $this->_controller;
+	}
+	/**
+	 * @return string
+	 */
+	public function getAction() 
+	{
+		return $this->_action;
+	}
+}
