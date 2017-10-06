996592cf5876a5021e976d1d5d16ed5151848062
onlynks <nicogar12@gmail.com>
2017-10-06 14:00:50 +0000

    we need to use PHP template in order to pass custom data to the view

---
+++ Framework/Controller.php
@@ -0,0 +1,21 @@
+<?php
+namespace Framework;
+class Controller {
+	protected $_view;
+	public function __construct() {
+		$router      = Registry::get( "router" );
+		$this->_view = new View( array(
+			"file" => APP_PATH . "/app/views/{$router->getController()}/{$router->getAction()}.php",
+		) );
+	}
+	/**
+	 * Renders the view
+	 */
+	public function render() {
+		header( "Content-type: text/html" );
+		include $this->_view->getViewFile();
+	}
+	public function __destruct() {
+		$this->render();
+	}
+}
---
+++ Framework/View.php
@@ -0,0 +1,28 @@
+<?php
+namespace Framework;
+class View {
+	protected $_file;
+	public function __construct( $options = array() ) {
+		foreach ( $options as $key => $value ) {
+			$method        = "_" . $key;
+			$this->$method = $value;
+		}
+	}
+	/**
+	 * Get view content from the file
+	 *
+	 * @return string | null
+	 */
+	public function getViewFile() {
+		if ( file_exists( $this->_file ) ) {
+			return $this->_file;
+		}
+		return null;
+	}
+	/**
+	 * @param string $file
+	 */
+	public function setFile( $file ) {
+		$this->_file = $file;
+	}
+}
