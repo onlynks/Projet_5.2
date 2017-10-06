a949602c0e6fe68955ba10068e3fd039258bf263
onlynks <nicogar12@gmail.com>
2017-10-06 14:28:48 +0000

    instanciate the View object in the Controller

---
+++ Framework/Controller.php
@@ -0,0 +1,9 @@
+<?php
+namespace Framework;
+class Controller {
+	protected $_view;
+	public function __construct() {
+		$router      = Registry::get( "router" );
+		$this->_view = new View();
+	}
+}
